3613306236a055b17ba78036a7807724fd116424
Stephen Ang <angssjr@g.ucla.edu>
2020-08-10 00:47:37 -0700

    1

--- hw6.js
+++ hw6.js
@@ -21,63 +21,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
-app.get('/', (req,res)=>{
-    console.log('GET! hello world!');
-    var qParams = [];
-    for(var p in req.query){
-        qParams.push({"name":p,"value":req.query[p]})
-    }   
-    var context = {};
-    context.dataList = qParams;
-    res.render('home',context);
-})
-
-app.post('/', function(req,res){
-    console.log("POST!");
-    var qParams = [];
-    for (var p in req.query){
-        qParams.push({"name":p,"value":req.query[p]})
-      }
-    for (var p in req.body){
-      qParams.push({"name":p,"value":req.body[p]})
-    }
-    console.log(qParams);
-    console.log(req.query);
-    console.log(req.body);
-    
-    var context = {};
-    context.dataList = qParams;
-    res.render('home', context);
-  });
-
-  
-app.get('/reset-table',function(req,res,next){
-  var context = {};
-  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
-    var createString = "CREATE TABLE workouts("+
-    "id INT PRIMARY KEY AUTO_INCREMENT,"+
-    "name VARCHAR(255) NOT NULL,"+
-    "reps INT,"+
-    "weight INT,"+
-    "date DATE,"+
-    "lbs BOOLEAN)";
-    pool.query(createString, function(err){
-      context.results = "Table reset";
-      res.render('home',context);
-    })
-  });
-});
-
-
-
-
-
-app.listen(app.get('port'),function(){
-    console.log('Express started on http://flip3.engr.oregonstate.edu'+app.get('port')+'; press Ctrl-C to terminate.')
-});
-    
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
 
   var ptr = document.getElementsByTagName("body");
@@ -151,5 +94,66 @@ document.addEventListener('DOMContentLoaded', (event) => {
   
 
 });
+
+
+
+
+
+app.get('/', (req,res)=>{
+    console.log('GET! hello world!');
+    var qParams = [];
+    for(var p in req.query){
+        qParams.push({"name":p,"value":req.query[p]})
+    }   
+    var context = {};
+    context.dataList = qParams;
+    res.render('home',context);
+})
+
+app.post('/', function(req,res){
+    console.log("POST!");
+    var qParams = [];
+    for (var p in req.query){
+        qParams.push({"name":p,"value":req.query[p]})
+      }
+    for (var p in req.body){
+      qParams.push({"name":p,"value":req.body[p]})
+    }
+    console.log(qParams);
+    console.log(req.query);
+    console.log(req.body);
+    
+    var context = {};
+    context.dataList = qParams;
+    res.render('home', context);
+  });
+
+  
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+
+
+
+
+app.listen(app.get('port'),function(){
+    console.log('Express started on http://flip3.engr.oregonstate.edu'+app.get('port')+'; press Ctrl-C to terminate.')
+});
+    
+
 
 
